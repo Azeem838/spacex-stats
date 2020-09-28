49a3c76422a35efa8f5455e691c191b542253353
Azeem <azeemahmed101@gmail.com>
2020-09-28 20:17:48 +0200

    add latest_launches reducer to update state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,22 @@
-import { combineReducers } from 'redux';
-import launch from './launch';
+const initialState = {
+  data: {},
+};
 
-const rootReducer = combineReducers({ launch });
+const rootReducer = (state = initialState, action) => {
+  if (action.type === 'LATEST_LAUNCHES') {
+    return {
+      ...state,
+      data: action.data,
+    };
+  }
+  return {
+    ...state,
+  };
+};
+
+// import { combineReducers } from 'redux';
+// import launch from './launch';
+
+// const rootReducer = combineReducers({ launch });
 
 export default rootReducer;
