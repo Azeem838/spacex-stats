2ac12cb0c2d3623fa8a8ed675296e709fe66dea7
Azeem <azeemahmed101@gmail.com>
2020-09-28 20:17:10 +0200

    update card component to use props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 
-const CardComponent = () => (
-  <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="holder.js/100px180" />
-    <Card.Body>
-      <Card.Title>Card Title</Card.Title>
-      <Card.Text>
-        Some quick example text to build on the card title and make up the bulk
-        of the cards content.
-      </Card.Text>
-      <Button variant="primary">More Information</Button>
-    </Card.Body>
-  </Card>
-);
+const CardComponent = ({ data }) => {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img
+        variant="top"
+        src={
+          data.links.flickr.small[0]
+            ? data.links.flickr.small[0]
+            : data.links.patch.small
+        }
+      />
+      <Card.Body>
+        <Card.Title>{data.name}</Card.Title>
+        <Card.Text>{data.details.substr(0, 150)}...</Card.Text>
+        <Button variant="primary">Read More</Button>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default CardComponent;
