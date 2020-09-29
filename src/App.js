d84b798903a8a83d06d87c23865b063e7a5d1780
Azeem <azeemahmed101@gmail.com>
2020-09-29 15:39:41 +0200

    add routes for each container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import NavComponent from './containers/Navbar';
 import CardList from './containers/CardList';
+import Dragons from './containers/Dragons';
+import Landpads from './containers/Landpads';
+import Rockets from './containers/Rockets';
+import Ships from './containers/Ships';
+import Launches from './containers/Launches';
+import Launch from './containers/Launch';
 
 function App() {
   return (
-    <div className="App">
-      <NavComponent />
-      <CardList />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavComponent />
+        <Route exact path="/" component={CardList} />
+        <Route exact path="/" component={Launches} />
+        <Route path="/dragons" component={Dragons} />
+        <Route path="/landpads" component={Landpads} />
+        <Route path="/rockets" component={Rockets} />
+        <Route path="/ships" component={Ships} />
+        <Route path="/:launch_id" component={Launch} />
+      </div>
+    </BrowserRouter>
   );
 }
 
