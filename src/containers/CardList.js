a43e87531eb3d92b5e6cfdcfb490c7da62cef448
Azeem <azeemahmed101@gmail.com>
2020-09-29 15:42:11 +0200

    update cardList to handle launches array and slipt into prevLaunches and upcoming

--- src/containers/CardList.js
+++ src/containers/CardList.js
@@ -9,16 +9,27 @@ class CardList extends Component {
   }
 
   render() {
-    console.log(this.props.data);
-    if (Object.getOwnPropertyNames(this.props.data).length !== 0) {
-      return <Card data={this.props.data} />;
+    if (Object.getOwnPropertyNames(this.props.launches).length !== 0) {
+      const prevLaunches = this.props.launches
+        .filter((launch) => {
+          return launch.upcoming === false;
+        })
+        .slice(1)
+        .slice(-10);
+      const upcomingLaunches = this.props.launches.filter((launch) => {
+        return launch.upcoming === true;
+      });
+      const cardList = prevLaunches.map((launch) => (
+        <Card data={launch} key={launch.id} />
+      ));
+      return <div className="container d-flex">{cardList}</div>;
     }
     return <div>Loading ...</div>;
   }
 }
 
 const mapStateToPropes = (state) => ({
-  data: state.data,
+  launches: state.launches,
 });
 
 export default connect(mapStateToPropes, actionCreators)(CardList);
