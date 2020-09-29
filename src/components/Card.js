5b0b780a500c725eca6a54ddc460921fc8db2f9f
Azeem <azeemahmed101@gmail.com>
2020-09-29 18:44:33 +0200

    make card hoverable and select the correct image from API

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const CardComponent = ({ data }) => {
   return (
-    <div className="col s12 m6">
+    <div className="col s12 m6 hoverable">
       <div className="card horizontal">
         <div className="card-image">
           <img
             alt="Launch"
             src={
-              data.links.flickr.small[0]
-                ? data.links.flickr.small[0]
+              data.links.flickr.original[0]
+                ? data.links.flickr.original[0]
                 : data.links.patch.small
             }
           />
@@ -18,7 +18,7 @@ const CardComponent = ({ data }) => {
         <div className="card-stacked">
           <div className="card-content">
             <span className="card-title">{data.name}</span>
-            <p>{data.details.substr(0, 150)}...</p>
+            <p>{data.details.substr(0, 50)}...</p>
           </div>
           <div className="card-action">
             <Link to={'/' + data.id}>Read More</Link>
