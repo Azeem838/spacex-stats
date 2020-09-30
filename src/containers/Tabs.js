3163110928ac1ec8a7653b76c30dca5f2e09b4d1
Azeem <azeemahmed101@gmail.com>
2020-09-30 09:58:31 +0200

    add filter tabs

---
+++ src/containers/Tabs.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Tabs = ({ rockets, handleFilter }) => {
+  const rocketNames = rockets.map((rocket, i) => (
+    <li className="tab" key={rocket.id}>
+      <a onClick={handleFilter} data-key={rocket.id} href={`#test'${i}`}>
+        {rocket.name}
+      </a>
+    </li>
+  ));
+  return (
+    <ul className="tabs tabs-fixed-width tab-demo z-depth-1">
+      <li className="tab" key="all">
+        <a onClick={handleFilter} data-key="all" href="#all">
+          All
+        </a>
+      </li>
+      {rocketNames}
+      <li className="tab" key="upcoming">
+        <a onClick={handleFilter} data-key="upcoming" href="#upcoming">
+          Upcoming
+        </a>
+      </li>
+      <li className="tab" key="previous">
+        <a onClick={handleFilter} data-key="previous" href="#previous">
+          Previous
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default Tabs;
