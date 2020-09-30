392cc969541d12b5e688182af1878df2108913c5
Azeem <azeemahmed101@gmail.com>
2020-09-30 06:01:58 +0200

    update card styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,28 +3,30 @@ import { Link } from 'react-router-dom';
 
 const CardComponent = ({ data }) => {
   return (
-    <div className="col s12 m6 hoverable">
-      <div className="card horizontal">
-        <div className="card-image">
-          <img
-            alt="Launch"
-            src={
-              data.links.flickr.original[0]
-                ? data.links.flickr.original[0]
-                : data.links.patch.small
-            }
-          />
-        </div>
-        <div className="card-stacked">
-          <div className="card-content">
-            <span className="card-title">{data.name}</span>
-            <p>{data.details.substr(0, 50)}...</p>
+    <div className="col s12 m6">
+      <Link to={'/' + data.id}>
+        <div className="card horizontal hoverable">
+          <div className="card-image">
+            <img
+              alt="Launch"
+              src={
+                data.links.flickr.original[0]
+                  ? data.links.flickr.original[0]
+                  : data.links.patch.small
+              }
+            />
           </div>
-          <div className="card-action">
-            <Link to={'/' + data.id}>Read More</Link>
+          <div className="card-stacked">
+            <div className="card-content">
+              <span className="card-title">{data.name}</span>
+              <p className="black-text">{data.details.substr(0, 50)}...</p>
+            </div>
+            <div className="card-action red-text">
+              <div>Read More</div>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
