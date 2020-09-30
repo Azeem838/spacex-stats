ee75d41ea9fbe04d3732f9fba3eaad934c3720b0
Azeem <azeemahmed101@gmail.com>
2020-09-30 06:00:52 +0200

    add switch from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NavComponent from './containers/Navbar';
-import CardList from './containers/CardList';
 import Dragons from './containers/Dragons';
 import Landpads from './containers/Landpads';
 import Rockets from './containers/Rockets';
 import Ships from './containers/Ships';
 import Launches from './containers/Launches';
 import Launch from './containers/Launch';
+import Home from './containers/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <NavComponent />
-        <Route exact path="/" component={CardList} />
-        <Route exact path="/" component={Launches} />
-        <Route path="/dragons" component={Dragons} />
-        <Route path="/landpads" component={Landpads} />
-        <Route path="/rockets" component={Rockets} />
-        <Route path="/ships" component={Ships} />
-        <Route path="/:launch_id" component={Launch} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/launches" component={Launches} />
+          <Route path="/dragons" component={Dragons} />
+          <Route path="/landpads" component={Landpads} />
+          <Route path="/rockets" component={Rockets} />
+          <Route path="/ships" component={Ships} />
+          <Route path="/:launch_id" component={Launch} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
