3dd38e94adc5dde8453faf3b1cda9f77a8501f59
Azeem <azeemahmed101@gmail.com>
2020-09-30 21:30:25 +0200

    add Tabs tests

--- src/containers/Tabs.js
+++ src/containers/Tabs.js
@@ -6,6 +6,7 @@ const Tabs = ({ rockets, handleFilter }) => {
     <li
       className={rocket.name === 'Starship' ? 'tab disabled' : 'tab'}
       key={rocket.id}
+      data-testid={`${rocket.name}-tab`}
     >
       <a onClick={handleFilter} data-key={rocket.id} href={`#${i}`}>
         {rocket.name}
@@ -14,18 +15,18 @@ const Tabs = ({ rockets, handleFilter }) => {
   ));
   return (
     <ul className="tabs tabs-fixed-width z-depth-1">
-      <li className="tab" key="all">
+      <li className="tab" key="all" data-testid="all-tab">
         <a onClick={handleFilter} data-key="all" href="#all">
           All
         </a>
       </li>
       {rocketNames}
-      <li className="tab" key="upcoming">
+      <li className="tab" key="upcoming" data-testid="upcoming-tab">
         <a onClick={handleFilter} data-key="upcoming" href="#upcoming">
           Upcoming
         </a>
       </li>
-      <li className="tab" key="previous">
+      <li className="tab" key="previous" data-testid="previous-tab">
         <a onClick={handleFilter} data-key="previous" href="#previous">
           Previous
         </a>
