df5ba96556c888993f4a91fa35bcc565872c29ba
Azeem <azeemahmed101@gmail.com>
2020-09-30 09:58:19 +0200

    refactor cardList to stateless component using props from launches page and implement filter

--- src/containers/CardList.js
+++ src/containers/CardList.js
@@ -1,41 +1,35 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import * as actionCreators from '../actions/index';
 import Card from '../components/Card';
 
-class CardList extends Component {
-  componentDidMount() {
-    this.props.loadLaunches();
-    this.props.loadRockets();
-  }
-
-  render() {
-    if (this.props.rockets.length !== 0) {
-      // console.log(this.props.rockets);
-    }
-
-    if (Object.getOwnPropertyNames(this.props.launches).length !== 0) {
-      const prevLaunches = this.props.launches
+const CardList = (props) => {
+  console.log(props.filter);
+  if (Object.getOwnPropertyNames(props.launches).length !== 0) {
+    let filterLaunches;
+    if (props.filter === '' || props.filter === 'all') {
+      filterLaunches = props.launches.slice(1).slice(-10);
+    } else if (props.filter === 'previous') {
+      filterLaunches = props.launches
         .filter((launch) => {
           return launch.upcoming === false;
         })
         .slice(1)
         .slice(-10);
-      const upcomingLaunches = this.props.launches.filter((launch) => {
+    } else if (props.filter === 'upcoming') {
+      filterLaunches = props.launches.filter((launch) => {
         return launch.upcoming === true;
       });
-      const cardList = prevLaunches.map((launch) => (
-        <Card data={launch} key={launch.id} />
-      ));
-      return <div className="container launch-contain d-flex">{cardList}</div>;
+    } else {
+      filterLaunches = props.launches.filter((launch) => {
+        return launch.rocket === props.filter;
+      });
     }
-    return <div>Loading ...</div>;
+    const cardList = filterLaunches.map((launch) => (
+      <Card data={launch} key={launch.id} />
+    ));
+
+    return <div className="container launch-contain">{cardList}</div>;
   }
-}
+  return <div>Loading ...</div>;
+};
 
-const mapStateToPropes = (state) => ({
-  launches: state.launches,
-  rockets: state.rockets,
-});
-
-export default connect(mapStateToPropes, actionCreators)(CardList);
+export default CardList;
