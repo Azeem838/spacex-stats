6910e71a4e52b259fcb7e486d68244ec3347faa7
Azeem <azeemahmed101@gmail.com>
2020-09-30 09:59:30 +0200

    update launches page to handle CardList, filter and tabs

--- src/pages/Launches.js
+++ src/pages/Launches.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/index';
 import CardList from '../containers/CardList';
+import Tabs from '../containers/Tabs';
 
-const Launches = () => (
-  <div>
-    <CardList />
-  </div>
-);
+class Launches extends Component {
+  state = {
+    filter: '',
+  };
 
-export default withRouter(Launches);
+  componentDidMount() {
+    this.props.loadLaunches();
+    this.props.loadRockets();
+  }
+
+  handleFilter = (e) => {
+    const filter = e.target.getAttribute('data-key');
+    this.setState({
+      filter,
+    });
+  };
+
+  render() {
+    if (this.props.rockets.length > 1) {
+      return (
+        <div>
+          <Tabs handleFilter={this.handleFilter} rockets={this.props.rockets} />
+          <CardList filter={this.state.filter} launches={this.props.launches} />
+        </div>
+      );
+    }
+    return <div>Loading...</div>;
+  }
+}
+
+const mapStateToPropes = (state) => ({
+  launches: state.launches,
+  rockets: state.rockets,
+});
+
+export default connect(mapStateToPropes, actionCreators)(Launches);
