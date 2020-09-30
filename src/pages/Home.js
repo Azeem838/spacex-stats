ebf3cd5a57170cc64cf3e75a771da3aa85b6f9ca
Azeem <azeemahmed101@gmail.com>
2020-09-30 11:56:17 +0200

    add homePark markup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/index';
 
-const Home = () => <h1 className="center">Home</h1>;
+class Home extends Component {
+  componentDidMount() {
+    this.props.loadRockets();
+  }
 
-export default Home;
+  render() {
+    const images = this.props.rockets.map((rocket) => {
+      return (
+        <div className="img__wrap">
+          <img
+            className="img__img"
+            src={rocket.images[0]}
+            alt={rocket.name}
+            key={rocket.id + rocket.name}
+          />
+          <p className="img__description">
+            <div className="img__text">{rocket.name}</div>
+          </p>
+        </div>
+      );
+    });
+
+    return (
+      <div className="card-panel grey lighten-2 jumbotron">
+        <div className="container">
+          <h1>SpaceX Stats</h1>
+          <p>
+            View your favourite SpaceX launches, filter by your favourite rocket
+            and relive History!
+          </p>
+          <div className="home-images">{images}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  rockets: state.rockets,
+});
+
+export default connect(mapStateToProps, actionCreators)(Home);
