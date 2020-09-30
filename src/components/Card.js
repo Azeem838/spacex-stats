76d357daa8ec55ac635c3577a4feb6a75aeaf734
Azeem <azeemahmed101@gmail.com>
2020-09-30 09:57:20 +0200

    update card to display default image and deal with null details

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../images/default-launch-image.jpg';
 
 const CardComponent = ({ data }) => {
   return (
@@ -13,13 +14,20 @@ const CardComponent = ({ data }) => {
                 data.links.flickr.original[0]
                   ? data.links.flickr.original[0]
                   : data.links.patch.small
+                  ? data.links.patch.small
+                  : logo
               }
             />
           </div>
           <div className="card-stacked">
             <div className="card-content">
               <span className="card-title">{data.name}</span>
-              <p className="black-text">{data.details.substr(0, 50)}...</p>
+              <p className="black-text">
+                {data.details
+                  ? data.details.substr(0, 50)
+                  : 'No details available'}
+                ...
+              </p>
             </div>
             <div className="card-action red-text">
               <div>Read More</div>
