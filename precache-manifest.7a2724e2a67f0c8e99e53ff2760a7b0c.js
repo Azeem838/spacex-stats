3cefff4aa6ad4e29186ac2e802ff0ee58c4dcde4
Azeem <azeemahmed101@gmail.com>
2020-10-08 08:43:03 +0200

    Updates

---
+++ precache-manifest.7a2724e2a67f0c8e99e53ff2760a7b0c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bee7c5195f6308713a1418134caee3c",
+    "url": "/spacex-stats/index.html"
+  },
+  {
+    "revision": "bf282400e9069a3959c6",
+    "url": "/spacex-stats/static/css/main.396dd224.chunk.css"
+  },
+  {
+    "revision": "97fbe4e75883c9ca910b",
+    "url": "/spacex-stats/static/js/2.15f6e22a.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/spacex-stats/static/js/2.15f6e22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01d1472abed597074d3c",
+    "url": "/spacex-stats/static/js/3.d3d6acda.chunk.js"
+  },
+  {
+    "revision": "6d8f477b7be612fccf18",
+    "url": "/spacex-stats/static/js/4.0543f020.chunk.js"
+  },
+  {
+    "revision": "8e7fa176b006150306288bd092a696c0",
+    "url": "/spacex-stats/static/js/4.0543f020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e6424519b324f7ea870",
+    "url": "/spacex-stats/static/js/5.353cf10e.chunk.js"
+  },
+  {
+    "revision": "bf282400e9069a3959c6",
+    "url": "/spacex-stats/static/js/main.cb6e4b3b.chunk.js"
+  },
+  {
+    "revision": "458de328da9b12b15b0b",
+    "url": "/spacex-stats/static/js/runtime-main.8b79a33d.js"
+  },
+  {
+    "revision": "154191be7aa56bbe2dcfcbea9a06e287",
+    "url": "/spacex-stats/static/media/default-launch-image.154191be.jpg"
+  }
+]);
